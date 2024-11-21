109db9efa0ffd0708411b32f79737354a93c9983
MichaMendes <michellysilva@sptech.school>
2024-11-21 02:15:59 -0300

    tela EstoquePage(navTop, btns card)

--- src/pages/Estoque/EstoquePage.jsx
+++ src/pages/Estoque/EstoquePage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Sidebar from "../../components/SideBar/Sidebar";
-import styles from "./EstoquePage.module.css"
+import NavIntern from "../../components/NavIntern/NavIntern";
+import BtnAddProduct from "../../components/Button/BtnAddProduct";
+import BtnConfig from "../../components/Button/BtnConfig";
+import CardEstoque from "../../components/CardEstoque/CardEstoque";
+import styles from "./EstoquePage.module.css";
 
 const EstoquePage = () => {
     return (
@@ -9,7 +13,18 @@ const EstoquePage = () => {
                 <Sidebar />
             </div>
             <div className={styles.conteudo}>
-                <h1>ESTOQUE ESTOQUE ESTOQUE ESTOQUE ESTOQUE ESTOQUE ESTOQUE ESTOQUE</h1>
+                <div className={styles.title_page}>
+                    <h1>Gestão de Estoque</h1>
+                </div>
+                <div className={styles.navIntern_top}>
+                    <NavIntern />
+                </div>
+                <div className={styles.container_btn}>
+                    <BtnAddProduct /> <BtnConfig />
+                </div>
+                <div className={styles.container}>
+                    <CardEstoque/> <CardEstoque/> <CardEstoque/>
+                </div>
             </div>
         </div>
     );
